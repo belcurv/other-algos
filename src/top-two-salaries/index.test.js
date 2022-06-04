d4ed684ce5480f576598aa42d93cfce85a844587
Jay Schwane <jrschwane@uwalumni.com>
2022-06-03 20:49:45 -0500

    Add test cases

--- src/top-two-salaries/index.test.js
+++ src/top-two-salaries/index.test.js
@@ -1,6 +1,13 @@
 const { topTwoSalaries } = require('./index')
 
 describe('topTwoSalaries', () => {
+  it('should return [9, 8] from [1, 2, 3, 4, 5, 6, 7, 8, 9]', () => {
+    const salaries = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const expected = [9, 8]
+    const actual = topTwoSalaries(salaries)
+    expect(actual).toEqual(expected)
+  })
+
   it('should return [666, 222] from [1, 5, 222, 34, 66, 2, 90, 666]', () => {
     const salaries = [1, 5, 222, 34, 66, 2, 90, 666]
     const expected = [666, 222]
@@ -11,6 +18,13 @@ describe('topTwoSalaries', () => {
   it('should return [666, 222] from [666, 5, 222]', () => {
     const salaries = [666, 5, 222]
     const expected = [666, 222]
+    const actual = topTwoSalaries(salaries)
+    expect(actual).toEqual(expected)
+  })
+
+  it('should return [2, 1] from [1, 2, 2]', () => {
+    const salaries = [1, 2, 2]
+    const expected = [2, 1]
     const actual = topTwoSalaries(salaries)
     expect(actual).toEqual(expected)
   })
